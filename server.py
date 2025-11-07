215b4f31378d99a9b7cd6fdb5f019876d354d70c
Andrei Litvin <andy314@gmail.com>
2025-11-07 14:00:11 -0500

    Some initial version.

---
+++ server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "fastmcp",
+# ]
+# ///
+from fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    mcp.run()
