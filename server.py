e2115339b5ece240c2c4134a10b87a3eb91f0c5a
Andrei Litvin <andy314@gmail.com>
2025-11-07 14:36:58 -0500

    Prepare for more data.

--- server.py
+++ server.py
@@ -13,6 +13,7 @@ from fastmcp import FastMCP
 import glob
 import os
 
+
 class MatterSpecData:
     def __init__(self, spec_path: str):
         self.spec_path = spec_path
@@ -20,24 +21,45 @@ class MatterSpecData:
 
         # figure out all doc names
         for name in glob.glob(os.path.join(spec_path, "src", "**", "*.adoc")):
-            self.docs[name[len(spec_path):].strip('/')] = name
+            self.docs[name[len(spec_path) :].strip("/")] = name
 
-    def get_doc(name):
+    def get_doc(self, name):
+        with open(self.docs[name], "rt", encoding="utf8") as f:
+            return f.read()
+
+
+class MatterTestPlanData:
+    def __init__(self, spec_path: str):
+        self.spec_path = spec_path
+        self.docs = {}
+
+        # figure out all doc names
+        for name in glob.glob(os.path.join(spec_path, "src", "**", "*.adoc")):
+            self.docs[name[len(spec_path) :].strip("/")] = name
+
+    def get_doc(self, name):
         with open(self.docs[name], "rt", encoding="utf8") as f:
             return f.read()
 
 
 @click.command()
 @click.option(
-    "--matter-spec-path",
+    "--spec",
     type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True),
     help="Path to the Matter Specification folder.",
-    default=".",
+    default="~/devel/connectedhomeip-spec",
+    show_default=True,
+)
+@click.option(
+    "--test-plans",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True),
+    help="Path to the Matter Test plans folder.",
+    default="~/devel/chip-test-plans",
     show_default=True,
 )
 @click.option(
     "--transport",
-    type=click.Choice(['stdio', 'http']),
+    type=click.Choice(["stdio", "http"]),
     default="stdio",
     help="Transport protocol for the MCP server (e.g., http, websockets).",
     show_default=True,
@@ -56,20 +78,28 @@ class MatterSpecData:
     help="Port for the MCP server.",
     show_default=True,
 )
-def cli(matter_spec_path, transport, host, port):
+def cli(spec, test_plans, transport, host, port: int):
     mcp = FastMCP("Demo")
 
-    print(f"Running for Matter Specification Path: {matter_spec_path}")
-    matter_spec = MatterSpecData(matter_spec_path)
+    print(f"Matter Specification Path: {spec}")
+    print(f"Test plan path:            {test_plans}")
+    matter_spec = MatterSpecData(spec)
+    test_plan_data = MatterTestPlanData(spec)
 
-    @mcp.resource("data://spec/available_documents")
+    @mcp.resource("data://available/spec-docs")
     def get_spec_documents() -> list[str]:
         """
-        Get available specification documents.
+        Get available specification document names.
         """
         return list(matter_spec.docs.keys())
 
-    # Spec documentation path
+    @mcp.resource("data://available/test-plans")
+    def get_test_documents() -> list[str]:
+        """
+        Get available test plan document names.
+        """
+        return list(test_plan_data.docs.keys())
+
     @mcp.resource("spec-doc://{name}")
     def get_spec_data(name: str) -> str:
         """
@@ -77,10 +107,18 @@ def cli(matter_spec_path, transport, host, port):
         """
         return matter_spec.get_doc(name)
 
-    if transport == 'stdio':
+    @mcp.resource("test-plan://{name}")
+    def get_test_plan(name: str) -> str:
+        """
+        Get a specific test plan document.
+        """
+        return test_plan_data.get_doc(name)
+
+    if transport == "stdio":
         mcp.run(transport=transport)
     else:
         mcp.run(transport=transport, host=host, port=port)
+
 
 if __name__ == "__main__":
     cli()
